9f22f63f7946def10d369e0df6aa4f12fcf538c2
Devon Wijesinghe <devon@Devons-MacBook-Pro.local>
2019-04-23 11:12:11 +0530

    adding changes to tutorial

--- GastbyFrontend/src/templates/tutorial.js
+++ GastbyFrontend/src/templates/tutorial.js
@@ -9,8 +9,8 @@ import BlockContent from '@sanity/block-content-to-react';
 export const query = graphql`
   query($slug: String) {
     sanityTutorial(slug: { current: { eq: $slug } }) {
-      title,
-      description,
+      title
+      description
       mainImage {
         asset {
           fluid {
@@ -18,23 +18,54 @@ export const query = graphql`
           }
         }
       }
+      body {
+        _key
+        _type
+        style
+        list
+        children {
+          _key
+          _type
+          text
+        }
+      }
     }
   }
 `;
 
-export default ({ data }) => (
-  <MainLayout>
-    <PageHeader
-      onBack={() => { navigateTo('tutorials/overview')}}
-      title={"Back"}
-    />
-    <h1>{data.sanityTutorial.title}</h1>
-    <p>{data.sanityTutorial.description}</p>
-    <Image
-      fluid={data.sanityTutorial.mainImage.asset.fluid}
-      alt={data.sanityTutorial.title}
-      style={{ width: 800 }}
-    />
-    <BlockContent blocks={data.sanityTutorial.body}/>,
-  </MainLayout>
-);
+const serializers = {
+  types: {
+    code: props => (
+      <pre data-language={props.node.language}>
+        <code>{props.node.code}</code>
+      </pre>
+    )
+  }
+}
+
+export default ({ data }) => {
+  //data.sanityTutorial.body[0].markDefs = [];
+  return (
+    <MainLayout>
+      <PageHeader
+        onBack={() => { navigateTo('tutorials/overview') }}
+        title={"Back"}
+      />
+      <h1>{data.sanityTutorial.title}</h1>
+      <p>{data.sanityTutorial.description}</p>
+      <Image
+        fluid={data.sanityTutorial.mainImage.asset.fluid}
+        alt={data.sanityTutorial.title}
+        style={{ width: 800 }}
+      />
+      {console.log(data.sanityTutorial.body)}
+      <BlockContent
+        blocks={data.sanityTutorial.body}
+        serializers={serializers}
+        imageOptions={{ w: 320, h: 240, fit: 'max' }}
+        projectId="vi3cop8z"
+        dataset="production"
+      />
+    </MainLayout>
+  )
+};
